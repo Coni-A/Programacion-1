d79618e376cb08e4a9207c29ca567dda92622870
Coni <constanza.a.escri@gmail.com>
2026-08-14 17:18:09 -0300

    empece con algo del codigo no llegue a mucho, despues sigo

---
+++ TPO_Grupo2.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+# Funciones Principales
+""" El inventario llevara el siguiente orden de caracteristicas: [codigo, sub categoria, nombre, fecha de vencimiento, costo, cantidad]"""
+
+inventario = [[123456789, 'lacteos', 'Leche entera la Scerenisima', '10/09/2027', 3000, 200]]
