bea34b53032ccad07bb5734d32c69ab8748825ad
Coni <constanza.a.escri@gmail.com>
2026-08-21 15:05:24 -0300

    Agregue funciones de validacion

--- README.md
+++ README.md
@@ -7,3 +7,30 @@ Sistema de gestion de inventarios:
 - cantidad
 - nombre del producto o articulo
 - tipo de producto (secciones o sub categorias ej: lacteos, higiene, etc.)
+
+*modulos necesarios para el sistema*
+- fecha de vencimiento:datetime (comparacion fecha de vencimiento con fecha actual)
+
+*Funciones necesarias para el sistema*
+- funcion que imprima el inventario ((funciona)) 
+
+- funcion que permita dar de baja productos, el decir reducir la cantidad que quedan del mismo y en caso de que su cantidad sea igual a 0 se lo elimine
+
+- funcion para agregar productos al inventario (debe de agragar todas las categorias de forma obligatoria). (funciona)
+
+- funcion de busqueda de un producto en especifico por nombre o codigo
+
+- funcion que imprima todos los productos "prontos a vencer" (vencimiento cercano, se debe elegir una fecha a futuro de la actual por ejemplo en los proximos tres meses)
+
+- funcion impresion por categorias, se debe poder imprimir el stock disponible por sub categoria 
+
+*Funciones respecto a los input*
+
+- funcion verificacion es numero (funciona)
+- funcion verificacion es alphabetico
+- funcion verificacion no es vacio (funciona)
+- funcion verificacion de codigo (funciona)
+
+
+
+
--- TPO_Grupo2.py
+++ TPO_Grupo2.py
@@ -1,19 +1,73 @@
 from datetime import datetime
 
 # Funciones Principales
-""" El inventario llevara el siguiente orden de caracteristicas: [codigo, sub categoria, nombre, fecha de vencimiento, costo, cantidad]"""
 
-inventario = [[123456789, 'lacteos', 'Leche entera la Scerenisima por 1l', '10/09/2027', 3000, 200],
-              [987654321, 'lacteos', 'Queso muzzarela La Blanca por 500gm', '13/12/2027', 7000, 120]]
 
-def imprimir_inventario():
+
+def crear_inventario():
+    """Crea y devuelve el inventario inicial. El inventario llevara el siguiente orden de caracteristicas: [codigo, sub categoria, nombre, fecha de vencimiento, costo, cantidad"""
+    return [[123456789, 'lacteos', 'Leche entera la Scerenisima por 1l', '2027/12/30', 3000, 200],
+            [987654321, 'lacteos', 'Queso muzzarela La Blanca por 500gm', '2027/09/20', 7000, 120]]
+
+
+def validar_no_es_vacio(cadena):
+    """Valida que la cadena ingresada no esté vacía"""
+
+    while cadena == "": 
+        cadena = input("El valor no puede estar vacío. Ingrese nuevamente: ")
+    return cadena
+
+def validar_numero(num):
+    """Valida que el numero ingresado sea positivo y valido"""
+
+    num = validar_no_es_vacio(num)
+    while not num.isdigit() or int(num) <= 0:
+        num = input("Ingrese un numero positivo y valido: ")
+    return int(num)
+
+def validar_codigo(cod):
+    "Valida que el codigo de identificacion tenga los 9 caracteres necesarios"
+    cod = validar_no_es_vacio(cod)
+    while len(cod) != 9:
+        cod = input("Ingrese un codigo de nueve (9) caracteres: ")
+    return(cod)
+    
+def validar_fecha(fecha):
+    """Valida que la fecha ingresada esté en el formato AAAA/MM/DD y no sea una fecha pasada"""
+
+    fecha = validar_no_es_vacio(fecha)
+    valido = False
+    while not valido:
+        if len(fecha) == 10 and fecha[4] == '/' and fecha[7] == '/' and fecha[:4].isdigit() and fecha[5:7].isdigit() and fecha[8:].isdigit():
+
+            anio = int(fecha[:4])
+            mes = int(fecha[5:7])
+            dia = int(fecha[8:])
+
+            if 1 <= mes <= 12 and 1 <= dia <= 31:
+                hoy = datetime.now()
+                hoy_anio = int(str(hoy)[:4])
+                hoy_mes = int(str(hoy)[5:7])
+                hoy_dia = int(str(hoy)[8:10])
+
+                if (anio > hoy_anio) or (anio == hoy_anio and mes > hoy_mes) or (anio == hoy_anio and mes == hoy_mes and dia >= hoy_dia):
+                    valido = True
+                    continue
+        fecha = input("Error, formato de fecha inválido o fecha pasada. Ingreselo nuevamente (AAAA/MM/DD): ")
+
+    return fecha
+
+def imprimir_inventario(inventario):
+    "la funcion imprimir_inventario muestra por terminal todos los productos, con sus categorias, que se encuentren en el inventario"
     print("Productos registrados en el inventario:")
     for i, producto in enumerate(inventario):
+        print('\n')
         print(i+1,". Codigo de identificacion: ", producto[0],'\n' " Categoria: ", producto[1], '\n' " Nombre : ", producto[2],'\n' " Fecha de vencimiento: ", producto[3],'\n' " Costo: $", producto[4], '\n'" Cantidad disponibles: ", producto[5])
     if inventario == [ ]:
         print("No hay productos registrados en el inventario.")
 
-def agregar_producto(codigo, sub_categoria, nombre, fecha_de_vencimiento, costo, cantidad):
+def agregar_producto(inventario, codigo, sub_categoria, nombre, fecha_de_vencimiento, costo, cantidad):
+    "Agrega un producto nuevo al inventario"
     producto = [codigo, sub_categoria, nombre, fecha_de_vencimiento, costo, cantidad]
     inventario.append(producto)
     print("Se a agregado el producto.",producto[2],"con éxito.")
@@ -22,35 +76,32 @@ def mostrar_menu():
     print("Menu de inicio")
     print("1. Mostrar inventario")
     print("2. Agregar producto")
+    print("3. Salir")
 
 
-
-
-
+# Programa principal 
+inventario = crear_inventario()
 print("\n")
 print("Sistema de gestion de inventario")
 mostrar_menu()
-opcion = input("Elija una opción: ")
-while opcion != "3":
+opcion = validar_numero(input("Elija una opción: "))
+while opcion != 3:
 
-    if opcion == "1":
-        imprimir_inventario()
+    if opcion == 1:
+        imprimir_inventario(inventario)
 
-    elif opcion == "2":
-        codigo = input("Ingrese el codigo del producto: ")
+    elif opcion == 2:
+        codigo_producto = validar_codigo(input("Ingrese el codigo del producto: "))
         categoria = input("Ingrese la categoria del producto: ")
-        nombre= input("Ingrese el nombre del producto: ")
-        fecha_ven = input("Ingrese la fecha de vencimiento del producto (DD:MM:AAAA): ")
-        costo = int(input("Ingrese el costo del producto: "))
-        cantidad = int(input("Ingrese la cantidad de articulos disponibles: "))
-        agregar_producto(codigo, categoria, nombre, fecha_ven, costo, cantidad)
-
-    elif opcion == "3":
-        print("Programa finalizado")
-
+        nombre_producto = input("Ingrese el nombre del producto: ")
+        fecha_ven = validar_fecha(input("Ingrese la fecha de vencimiento del producto (AAAA/MM/DD): "))
+        costo = validar_numero(input("Ingrese el costo del producto: "))
+        cantidad_disponible = validar_numero(input("Ingrese la cantidad de articulos disponibles: "))
+        agregar_producto(inventario, codigo_producto, categoria, nombre_producto, fecha_ven, costo, cantidad_disponible)
     else:
         print("Opción inválida. Intente nuevamente.")
 
     mostrar_menu()
-    opcion = input("Elija una opción: ") 
+    opcion = validar_numero(input("Elija una opción: ")) 
+print("Programa finalizado")
 
